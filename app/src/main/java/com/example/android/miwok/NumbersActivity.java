88ff07cf60eb0f2daa04854c534a1070d0e0ceb7
Aggrey Ochieng <aggreyah@gmail.com>
2018-08-30 23:58:42 +0300

    Video five lesson 3 quiz

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,8 +15,9 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +25,28 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String[] words = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        Log.v("NumbersActivity", "Word at index 1: " + words[0]);
+        Log.v("\nNumbersActivity", "Word at index 2: " + words[1]);
+        Log.v("\nNumbersActivity", "Word at index 3: " + words[2]);
+        Log.v("\nNumbersActivity", "Word at index 4: " + words[3]);
+        Log.v("\nNumbersActivity", "Word at index 5: " + words[4]);
+        Log.v("\nNumbersActivity", "Word at index 6: " + words[5]);
+        Log.v("\nNumbersActivity", "Word at index 7: " + words[6]);
+        Log.v("\nNumbersActivity", "Word at index 8: " + words[7]);
+        Log.v("\nNumbersActivity", "Word at index 9: " + words[8]);
+        Log.v("\nNumbersActivity", "Word at index 10: " + words[9]);
     }
 }
